8bb7800fb6298785b5b11de90fd0ea6cc0f14866
James McCreight <jamesmcc@ucar.edu>
2015-01-19 10:30:26 -0700

    Fixed doc issues with utilities, GetPkgDataPath

--- R/utilities.R
+++ R/utilities.R
@@ -2,7 +2,7 @@
 #' 
 #' \code{GetPkgData} is a simplified wrapper (for system.file) for loading external rwrfhydro data included with the package.
 #' 
-#' @param file The external data file to load (this is in dirrefent places before, rwrfhydro/inst/extdata, and after build, rwrfhydro/). 
+#' @param theFile The external data file to load (this is in dirrefent places before, rwrfhydro/inst/extdata, and after build, rwrfhydro/). 
 #' @return The full path to the file.
 #' @examples
 #' GetPkgDataPath('Fourmile_test_case_AD.hydro_OrodellBasin_100m_8.nc')
