2e22e23998d82c3baba619503b29f2fb413fc765
James McCreight <jamesmcc@ucar.edu>
2015-01-19 17:19:16 -0700

    some work to improve visualize_channtwk.R, adding in finding nearest grid cells for given gauge locations and plotting them

--- R/visualize_channtwk.R
+++ R/visualize_channtwk.R
@@ -12,7 +12,9 @@
 #' GetChanPt <- VisualizeChanNtwk(fileCh)
 #' GetChanPt()
 #' @export
-VisualizeChanNtwk <- function(file) {
+VisualizeChanNtwk <- function(file, gaugePts=NULL) {
+
+  ## Get the data.
   ncid <- ncdf4::nc_open(file)
   
   if(length(grep('_Diag.nc',file))) {
@@ -24,7 +26,7 @@ VisualizeChanNtwk <- function(file) {
     linkDf <- data.frame( ind = 1:length(linkIndX) )
     linkDf$lon <- plyr::laply( linkDf$ind, function(ii) lon[linkIndX[ii],linkIndY[ii]] )
     linkDf$lat <- plyr::laply( linkDf$ind, function(ii) lat[linkIndX[ii],linkIndY[ii]] )
-    rm('lat','lon','linkIndX','linkIndY')
+    rm('linkIndX','linkIndY')
   } 
   
   if(length(grep('CHRTOUT',file))) {
@@ -36,17 +38,70 @@ VisualizeChanNtwk <- function(file) {
     linkDf$lon <- lon
     linkDf$lat <- lat
     linkDf$q <- q
-    rm('lat','lon')
   }
-  
-  thePlot <- 
-    ggplot2::ggplot( linkDf, ggplot2::aes(x=lon, y=lat, fill=q, label=ind) ) +
-    ggplot2::geom_raster() +
-    ggplot2::geom_text(color='magenta') + 
-    ggplot2::scale_x_continuous(expand=c(0,0)) +
-    ggplot2::scale_y_continuous(expand=c(0,0)) +
-    ggplot2::scale_fill_gradientn(colours=RColorBrewer::brewer.pal(9,'YlGnBu'))
+
+  ## standardize the lon just in case
+  stdLon <- function(x) {
+    x[which(x>180.)] <- x[which(x>180.)]-360.
+    x
+  }
+  linkDf$lon <- stdLon(linkDf$lon)
+
+  ## find nearest neighbors if gaugePts was defined.
+  if(length(gaugePts)) {
+    ## the euclidean metric in lat/lon works fine.
+    ## first, standardize the lon to +-180
+    gaugePts <- plyr::llply(gaugePts,
+                            function(ll) {ll$lon <- stdLon(ll$lon)
+                                          ll})
+    findNn <- function(ll) {
+      whMin <- which.min(sqrt( (ll$lon-linkDf$lon)^2 + (ll$lat-linkDf$lat)^2 ))
+      ll$chanInd <- whMin
+      ll$modelLon <- linkDf$lon[whMin]
+      ll$modelLat <- linkDf$lat[whMin]
+      ll$modelFile <- file
+      ll
+    }
+    gaugePts <- plyr::llply(gaugePts, findNn)
+
+    ## output the
+    printCustom <- function(ll) {
+      cat((names(gaugePts)[ll]),sep='\n')
+      print(t(gaugePts[[ll]]))
+    }
+    invisible(plyr::llply(1:length(gaugePts),printCustom))   
+   
+  }
+
+
+stop()  
+  ## create the plot object
+  location <- c(lon=mean(lon),lat=mean(lat))
+  zoom=13
+  source='google'
+  maptype='hybrid'
+
+  theMap <- ggmap::get_map(location, zoom = zoom, source = source, maptype=maptype)
+  library(ggplot2)
+  thePlot <-
+    ggmap::ggmap(theMap, extent='normal') +
+    ggplot2::geom_point( data=linkDf, ggplot2::aes(x=lon, y=lat) )
+ 
+    if(length(gaugePts)) {
+
+      gaugeDf <- plyr::ldply(gaugePts)
+
+      thePlot <- thePlot +
+        ggplot2::geom_point(data=plyr::ldply(gaugeDf),
+                            ggplot2::aes(x=lon, y=lat, shape=system, color=location))
+    }
+
+  ##    ggplot2::coord_quickmap() +
+  ##    ggplot2::scale_x_continuous(expand=c(0,0)) +
+  ##    ggplot2::scale_y_continuous(expand=c(0,0)) 
+  ##    ggplot2::scale_fill_gradientn(colours=RColorBrewer::brewer.pal(9,'YlGnBu'))
   print(thePlot)
+
   
   ## This function is going to be returned as a closure.
   ## It's env is visChanNtwkm which includes linkDf, and maybe the reference to the
