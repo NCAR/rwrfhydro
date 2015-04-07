1c07053a682d66cd0cc6feb06fd336fd58c8f73c
James L McCreight <mccreigh@gmail.com>
2015-04-06 22:24:24 -0600

    scripted parallel pulls of active gages, some diagnostics: 9822 supposedly active gages

--- R/real_time_usgs.R
+++ R/real_time_usgs.R
@@ -1,9 +1,11 @@
-if(FALSE){
-## want site status today
+#' @export
 GetActiveHucData <- function(huc, parameterCd=c('00060','00065')) {
   dataRetrieval::readNWISdata(service='iv', huc=huc, siteStatus='active', 
                               parameterCd=parameterCd)
 }
+
+if(FALSE){
+## want site status today
 
 allHuc <- NamedList(formatC(1:21,width = 2, flag='0'))
 require(doMC)
@@ -12,6 +14,19 @@ system.time(
   allActive <- plyr::llply(allHuc, GetActiveHucData, .parallel=TRUE)
 )
 ## took 10. mins on 4 processors 
+
+
+CollectPullStats <- function(huc){
+  load(paste0("~/huc",huc,'.RData'))
+  data.frame(time=pullTime['elapsed'], size=obSize[1], 
+             nStns=length(unique(data$site_no)) )
+}
+hucList <- NamedList(formatC(1:21,width=2,flag='0'))
+pullDf<-plyr::ldply(hucList,CollectPullStats)
+
+library(ggplot2)
+ggplot(pullDf, aes(x=time, y=size, color=.id)) + geom_point() + theme_bw()
+ggplot(pullDf, aes(x=time, y=nStns, color=.id)) + geom_point() + theme_bw()
 
 hucData <- dataRetrieval::readNWISdata(service='iv', huc='10190005', 
                                        siteStatus='inactive', 
