43abb71cbcd401beb9923f40c179e93299e025be
James McCreight <jamesmcc@ucar.edu>
2015-04-07 21:14:33 -0600

    update plot

--- R/real_time_usgs.R
+++ R/real_time_usgs.R
@@ -5,40 +5,27 @@ GetActiveHucData <- function(huc, parameterCd=c('00060','00065')) {
 }
 
 if(FALSE){
-## want site status today
 
-allHuc <- NamedList(formatC(1:21,width = 2, flag='0'))
-require(doMC)
-registerDoMC(4)
-system.time(
-  allActive <- plyr::llply(allHuc, GetActiveHucData, .parallel=TRUE)
-)
-## took 10. mins on 4 processors 
-
-
-hucList <- NamedList(formatC(1:21,width=2,flag='0'))
-CollectPullStats <- function(huc){
-  theFile <- list.files(path='~/usgsStreamData/realTimeData/',
-                        pattern=paste0('huc',huc), full.names=TRUE)
-  print(theFile)
-  if(!file.exists(theFile[1])) return(data.frame(time=NA,size=NA, nSTns=NA))
-  load(theFile[1])
+allFiles <- list.files(path='/home/jamesmcc/usgsStreamData/realTimeData/', 
+                       pattern='.huc.*RData$', full.names=TRUE)
+  
+CollectPullStats <- function(file){
+  load(file)
   data.frame(time=pullTime['elapsed'], 
-             size=as.numeric(strsplit(obSize[1], ' ')[[1]][1]),
-             nStns=length(unique(data$site_no)) )
+             size=as.numeric(strsplit(obSize[1], '\\ ')[[1]][1]),
+             nStns=length(unique(data$site_no)), 
+             when=strsplit(file,'/|\\.')[[1]][7] )
 }
-pullDf<-plyr::ldply(hucList,CollectPullStats)
+pullDf<-plyr::ldply(allFiles,CollectPullStats)
+
+mPullDf <- reshape2::melt(pullDf, id=c('time','when'))
 
 library(ggplot2)
-ggplot(pullDf, aes(x=time, y=size, color=.id)) + geom_point() + theme_bw()
-ggplot(pullDf, aes(x=time, y=nStns, color=.id)) + geom_point() + theme_bw()
+ggplot(mPullDf, aes(x=time, y=value, color=when, group=when)) + 
+  geom_point(size=5) + 
+  geom_line() +
+  facet_wrap(~variable,ncol=1, scales='free_y') +
+  theme_bw(base_size=25)
 
-hucData <- dataRetrieval::readNWISdata(service='iv', huc='10190005', 
-                                       siteStatus='inactive', 
-                                       parameterCd=c('00060','00065'))
 
-
-hucData <- dataRetrieval::readNWISdata(service='iv', sites=c('06724970','06730160'),
-                                       siteStatus='active', 
-                                       parameterCd=c('00060','00065'))
 }
