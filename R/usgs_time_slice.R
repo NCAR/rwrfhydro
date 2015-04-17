34e5729e278d582d3a5cf4ca68c3bd425758a57c
James L McCreight <mccreigh@gmail.com>
2015-04-17 14:45:07 -0600

    minor prog on time slices

--- R/usgs_time_slice.R
+++ R/usgs_time_slice.R
@@ -1,55 +1,47 @@
-#' Make timeslices for data assimilation from realTimeData
+#' Make timeslices from active USGS data files. 
 #' 
 #' @examples
 #' realTimeFiles <- list.files(pattern='huc.*.RData', 
 #'                             path='~/usgsStreamData/realTimeData', 
 #'                             full.names=TRUE)
 #' outPath = '~/usgsStreamData/timeSliceData/'
-#' MkDaTimeSlice( realTimeFiles, outPath )
-
-#' allMins <- c()
-#' for(rr in realTimeFiles){
-#'  load(rr)
-#'  allMins <- 
-#'  #c(allMins, as.numeric(format(data$dateTime,'%M')))
-#'  c(allMins, as.numeric(format(lubridate(data$dateTime,'%M')))
-#'  (round(59 * .2) / .2) %% 60
-#' }
-#' table(allMins)
-
-  
-
-MkDaTimeSlice <- function( realTimeFiles, outPath ){
+#' MkUsgsTimeSlice( realTimeFiles, outPath )
+MkUsgsTimeSlice <- function( realTimeFiles, outPath, nearestMin=5,
+                             varianceFunction=NULL){
   ## sort realTimeFiles by their dates? not sure this is necessary.
   
   ## get back a list of output files for each input file
   ## this operation *cannot* be parallelized. 
   ## 
-  
-  inOutList <- plyr::llply(realTimeFiles[1], SliceHucFile, outPath=outPath)
+    
+  inOutList <- plyr::llply(realTimeFiles[1], SliceHucFile, 
+                           outPath=outPath, nearest=nearestMin, 
+                           varianceFunction=varianceFunction)
   
 }
 
-SliceHucFile <- function(file, outPath) {
+SliceHucFile <- function(file, outPath, nearest,
+                         varianceFunction) {
   load(file)
   #uniquePosix <- unique(data$dateTime)
-  ## this operation can be parallelized
+  ## this operation CAN be parallelized
   
-  
+  data$dateTime <- RoundMinutes(data$dateTime, nearest=nearest)
+  print(table(data$dateTime))
   outList <- plyr::ddply(data, plyr::.(dateTime), 
                          WriteTimeSlice, 
+                         outPath, varianceFunction, 
                          .parallel=(foreach::getDoParWorkers() > 1 ) )
   outList
 }
 
 
-WriteTimeSlice <- function(dfByPosix, outPath) {
+WriteTimeSlice <- function(dfByPosix, outPath, varianceFunction) {
   
-  str(dfByPosix)
-  if(unique())
-  file <- TimeSliceFileName()
+  #str(dfByPosix)
+  fileName <- TimeSliceFileName()
   ## does the file exist
-  if(file.exists()) {
+  if(file.exists(fileName)) {
     
   } else {
     
@@ -57,14 +49,19 @@ WriteTimeSlice <- function(dfByPosix, outPath) {
   stop()
 }
 
+
+#' Round a POSIXct time to the nearest mth minute.
 #' @examples
 #' POSIXct <- structure(c(1429296352.92171, 1429296366.12118, 1429296497.81269), class = c("POSIXct", "POSIXt"))
+#' RoundMinutes(POSIXct, nearest=5)
 RoundMinutes <- function(POSIXct, nearest=5) {
   nearestInv <- 1./nearest
-  POSIXct <- lubridate::round_date(POSIXct,'minute')
-  theMin <- as.numeric(format(POSIXct,'%M'))
+  theMin <- as.numeric(format(POSIXct,'%M')) + as.numeric(format(POSIXct,'%S'))/60
   roundMin <- (round(theMin * nearestInv) / nearestInv)
   diffMin <- roundMin - theMin
-  POSIXct + lubridate::minutes(diffMin)   
+  POSIXct + lubridate::seconds(round(diffMin*60))   
 }
+
+TimeSliceFileName <- function(POSIXct)
+  paste0(format(POSIXct,'%Y-%m-%d_%H:%M:%S'), '.usgsTimeSlice.ncdf')
 
