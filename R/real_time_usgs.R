91beb04651a26cc9dc57619de59c1f286d52765d
James McCreight <jamesmcc@ucar.edu>
2015-04-16 21:34:46 -0600

    diagnostics on realtime pulls

--- R/real_time_usgs.R
+++ R/real_time_usgs.R
@@ -15,21 +15,68 @@ allFiles <- list.files(path='/home/jamesmcc/usgsStreamData/realTimeData/',
   
 CollectPullStats <- function(file){
   load(file)
-  data.frame(time=pullTime['elapsed'], 
-             size=as.numeric(strsplit(obSize[1], '\\ ')[[1]][1]),
+  data.frame(huc02= strsplit(file,'\\.')[[1]][2],
+             time.sec=pullTime['elapsed'], 
+             size.MB=as.numeric(strsplit(obSize[1], '\\ ')[[1]][1]),
              nStns=length(unique(data$site_no)), 
              when=strsplit(file,'/|\\.')[[1]][7] )
 }
-pullDf<-plyr::ldply(allFiles,CollectPullStats)
+library(doMC)
+registerDoMC(25)
+pullDf<-plyr::ldply(allFiles,CollectPullStats, .parallel=TRUE)
 
-mPullDf <- reshape2::melt(pullDf, id=c('time','when'))
+mPullDf <- reshape2::melt(pullDf, id=c('time.sec','when','huc02'))
 
 library(ggplot2)
-ggplot(mPullDf, aes(x=time, y=value, color=when, group=when)) + 
-  geom_point(size=5) + 
-  geom_line() +
+ggplot(mPullDf, aes(x=time.sec, y=value, color=huc02)) + 
+  geom_text(size=3, aes(label=substr(huc02,4,5))) + 
+  #geom_line() +
   facet_wrap(~variable,ncol=1, scales='free_y') +
-  theme_bw(base_size=25)
-
-
+  theme_bw(base_size=24) 
+  
 }
+
+
+CollectLatency <- function(file){
+  load(file)
+  data.frame(file=file,
+             deltaT.min = as.numeric(data$dateTime - attr(data, 'queryTime')),
+             stringsAsFactors=FALSE)
+}
+
+latentDf <- plyr::ldply(allFiles, CollectLatency, .parallel=TRUE)
+
+nhrs <- 5
+nrow(latentDf)
+library(ggplot2)
+ggplot(subset(latentDf, abs(deltaT.min) < nhrs*60), 
+       aes(x=deltaT.min, y=..count../1135805 )) + 
+  geom_histogram(fill='lightblue', alpha=.5, binwidth=15, color='orange') + 
+  scale_x_continuous(breaks=15*( (-1*(nhrs*4)):0 ), 
+                     name='Time of obs relative to retrieval time (minutes)') +
+  scale_y_continuous(name='Fraction of all data in bin', breaks=.01*2*(0:13)) +
+  theme_bw()+
+  theme(panel.grid.major=element_line(size=.5))
+
+cdfDelta <- ecdf(latentDf$deltaT.min)
+cumProb  <- cdfDelta(seq(-135,0, by=15))
+names(cumProb) <- seq(-135,0, by=15)
+cumProb
+## yea! forecast observations are in the data set!
+binProb  <- diff(cdfDelta(seq(-135,0, by=15)))
+names(binProb) <- paste0(seq(-135,-15,by=15), ':', seq(-120,0,by=15))
+binProb
+
+realOld <- cdfDelta(-24*(3*(5:1)))
+names(realOld) <- -3*(5:1) ## days
+realOld
+
+## are stations only reported once per pull?
+CollectStnReps <- function(file){
+  load(file)
+  data.frame(file=file,
+             maxStnReps = max(table(data$site_no)))
+}
+
+repsDf <- plyr::ldply(allFiles, CollectStnReps, .parallel=TRUE)
+
