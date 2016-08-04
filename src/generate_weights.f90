0cbd615307bb4a53bba0e4a2b0d34d8b6de9b64a
logankarsten <karsten@hydro-c1.rap.ucar.edu>
2016-08-04 21:53:13 +0000

    Debugging

--- src/generate_weights.f90
+++ src/generate_weights.f90
@@ -217,8 +217,8 @@ implicit none
    srcArrayESMF = ESMF_ArrayCreate(farray=srcDummy,distgrid=srcDistGrid, &
                                    indexflag=ESMF_INDEX_GLOBAL, rc=ier)
    if(ier .ne. 0) return
-   print*, srcDummy
    !Initialize destination array to ndvSrc
+   print*, dstTemp
    dstArrayESMF = ESMF_ArrayCreate(farray=dstTemp,distgrid=dstDistGrid,indexflag=ESMF_INDEX_GLOBAL,&
                                    rc=ier)
    if(ier .ne. 0) return
