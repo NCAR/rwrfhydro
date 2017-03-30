09dde819c5855b70a5a61b0a196cf66f9af2dfb2
arezoorn <arezoo.rafieeinasab@gmail.com>
2017-03-30 16:16:02 -0600

    converted the reading to fixed width

--- R/get_USCRN.R
+++ R/get_USCRN.R
@@ -141,6 +141,9 @@
 #' 
 #' # get subhourly data only for basins in colorado
 #' outputsubhourly <- GetCRN(gageNames = "CO", elements = "PRECIPITATION", timestep = "subhourly", startDate = startDate, endDate= endDate)
+#' 
+#' # get hourly soik moisure data
+#' outputHourly <- GetCRN(gageNames = "CO", elements = c("SOIL_MOISTURE_5", "SOIL_MOISTURE_10", "SOIL_MOISTURE_20", "SOIL_MOISTURE_50", "SOIL_MOISTURE_100"), timestep = "hourly")
 
 #' @keywords 
 #' @concept 
@@ -151,46 +154,93 @@ GetCRN <- function(gageNames = NULL, siteIds= NULL, elements = NULL, timestep = 
                    startDate = Sys.time() - 10*24*3600, endDate = Sys.time(), realTime = FALSE, parallel = FALSE){
   
   # These addresses are fixed and need to be changes if NCEP changes their serving address.
-  urlSubHourly = "ftp://ftp.ncdc.noaa.gov/pub/data/uscrn/products/subhourly01/"
-  urlHourly = "ftp://ftp.ncdc.noaa.gov/pub/data/uscrn/products/hourly02/"
-  urlDaily = "ftp://ftp.ncdc.noaa.gov/pub/data/uscrn/products/daily01/"
-  urlMonthly = "ftp://ftp.ncdc.noaa.gov/pub/data/uscrn/products/monthly01/"
+  if (timestep == "subhourly") url_uscrn = "ftp://ftp.ncdc.noaa.gov/pub/data/uscrn/products/subhourly01/"
+  if (timestep == "hourly")    url_uscrn = "ftp://ftp.ncdc.noaa.gov/pub/data/uscrn/products/hourly02/"
+  if (timestep == "daily")     url_uscrn = "ftp://ftp.ncdc.noaa.gov/pub/data/uscrn/products/daily01/"
+  if (timestep == "monthly")   url_uscrn = "ftp://ftp.ncdc.noaa.gov/pub/data/uscrn/products/monthly01/"
   
   # Since the folders are arranged by yeas, so we need to find out which years shoul be processed.
   years <- seq(lubridate::year(startDate), lubridate::year(endDate))
   
-  if (timestep == "subhourly"){
+  # reading the hear information which is distributed in a separate file
+  headerInfo <- read.table(paste0(url_uscrn,"/HEADERS.txt"), stringsAsFactors = FALSE) 
+  
+  if (timestep == "monthly") {
+    urlPaths <- paste0(url_uscrn)
+  } else {
+    urlPaths <- paste0(url_uscrn,years,"/") 
+  }
+  
+  # get the name of all the files under the requested years
+  filenames <- foreach::"%do%"(foreach::foreach(url = urlPaths, .combine=function(...) data.table::rbindlist(list(...))), {
+    #  print(url)
+    filenames <- RCurl::getURL(url, dirlistonly = TRUE)
+    filenames <- paste0(url,strsplit(filenames, "\r*\n")[[1]])  # split the name of the files
+    return(filenames)
+  })
+  
+  # To remove other files rather than the CRN files
+  filenames <- unique(grep("CRN", filenames, value=TRUE))
+  
+  # if part of the file names has been given, find only those files and open those
+  if (!is.null(gageNames)) filenames <- unique(grep(paste(gageNames,collapse="|"), filenames, value=TRUE))
+  
+  ######---------------------> 
+  # function for reading the files according to the timeStep
+  
+  ReadTheFile <- function(file){
     
-    # reading the hear information which is distributed in a separate file
-    headerInfo <- read.table(paste0(urlSubHourly,"/HEADERS.txt"), stringsAsFactors = FALSE) 
-    
-    urlPaths <- paste0(urlSubHourly,years,"/")
-    
-    # get the name of all the files under the requested years
-    filenames <- foreach::"%do%"(foreach::foreach(url = urlPaths, .combine=function(...) data.table::rbindlist(list(...))), {
-      #  print(url)
-      filenames <- RCurl::getURL(url, dirlistonly = TRUE)
-      filenames <- paste0(url,strsplit(filenames, "\r*\n")[[1]])  # split the name of the files
-      return(filenames)
-    })
-    
-    # To remove other files rather than the CRN files
-    filenames <- unique(grep("CRN", filenames, value=TRUE))
-    
-    # if part of the file names has been given, find only those files and open those
-    if (!is.null(gageNames)) filenames <- unique(grep(paste(gageNames,collapse="|"), filenames, value=TRUE))
-    
-    GetsubHourlyFile <- function(file){
-      dat <- tryCatch(readr::read_table(file, col_names = FALSE, col_types = paste(c(rep('c', 6), rep('d', 17)), collapse = '')),
-                      error=function(cond) {message(cond); return(NA)})
-      names(dat) <- headerInfo[2,]
+    if (timestep == "subhourly"){
+      dat <- tryCatch(readr::read_fwf(file, 
+                                      col_positions = readr::fwf_positions(c(1, 7, 16, 21, 30, 35, 42, 50, 58, 66, 74, 81, 83, 91, 93, 95, 101, 103, 111, 119,
+                                                                             125, 127, 134),
+                                                                           c(5, 14, 19, 28, 33, 40, 48, 56, 64, 72, 79, 81, 89, 91, 93, 99, 101, 109, 117, 123,
+                                                                             125, 132, 134)),
+                                      col_types = paste(c(rep('c', 6), rep('d', 5),'cdccdcdddcdc'), collapse = '')),
+                      error=function(cond) {message(cond); return(NA)})                              
+    } else  if (timestep == "hourly") {
+      dat <- tryCatch(readr::read_fwf(file, 
+                                      col_positions = readr::fwf_positions(c(1, 7, 16, 21, 30, 35, 42, 50, 58, 66, 74, 82, 90, 98, 105, 107, 114, 116, 123, 125,
+                                                                             127, 135, 137, 145, 147, 155, 157, 163, 165, 173, 181, 189, 197, 205, 213, 221, 229, 237),
+                                                                           c(5, 14, 19, 28, 33, 40, 48, 56, 64, 72, 80, 88, 96, 103, 105, 112, 114, 121, 123, 125,
+                                                                             133, 135, 143, 145, 153, 155, 161, 163, 171, 179, 187, 195, 203, 211, 219, 227, 235, 243)),
+                                      col_types = paste(c(rep('c', 6), rep('d', 8),'cdcdccdcdcdcdc', rep('d',10)), collapse = '')),
+                      error=function(cond) {message(cond); return(NA)})        
+    } else  if (timestep == "daily")   {
+      dat <- tryCatch(readr::read_fwf(file, 
+                                      col_positions = readr::fwf_positions(c(1, 7, 16, 23, 31, 39, 47, 55, 63, 71, 79, 88, 90, 98, 106, 114, 122, 130, 138, 146, 154,
+                                                                             162, 170, 178, 186, 194, 202, 210),
+                                                                           c(5, 14, 21, 29, 37, 45, 53, 61, 69, 77, 86, 88, 96, 104, 112, 120, 128, 136, 144, 152, 160, 
+                                                                             168, 176, 184, 192, 200, 208, 216)),
+                                      col_types = paste(c(rep('c', 3), rep('d', 8),'c', rep('d',16)), collapse = '')),
+                      error=function(cond) {message(cond); return(NA)}) 
+    } else  if (timestep == "monthly") {
+      dat <- tryCatch(readr::read_fwf(file, 
+                                      col_positions = readr::fwf_positions(c(1, 7, 14, 21, 31, 41, 49, 57, 65, 73, 81, 89, 91, 99, 107),
+                                                                           c(5, 12, 19, 29, 39, 47, 55, 63, 71, 79, 87, 89, 97, 105, 113)),
+                                      col_types = paste(c('cccddddddddcddd'), collapse = '')),
+                      error=function(cond) {message(cond); return(NA)}) 
+    } else {
       
-      # only those with the requested siteIds to be selected
-      if (!is.null(siteIds)) dat <- subset(dat, WBANNO %in% siteIds )
+      stop("the step could only be one of the followings: subhourly, hourly, daily, monthly")
       
-      # only those in the requested domain to be selected
+    }
+    names(dat) <- headerInfo[2,]
+    
+    # only those with the requested siteIds to be selected
+    if (!is.null(siteIds)) dat <- subset(dat, WBANNO %in% siteIds )
+    
+    # only those in the requested domain to be selected
+    if (timestep == "monthly") {
+      if (!is.null(domain)) dat <- subset(dat, PRECISE_LONGITUDE >= domain$lonmin & PRECISE_LONGITUDE <= domain$lonmax 
+                                          & PRECISE_LATITUDE >= domain$latmin & PRECISE_LATITUDE <= domain$latmax)
+    } else {
+      
       if (!is.null(domain)) dat <- subset(dat, LONGITUDE >= domain$lonmin & LONGITUDE <= domain$lonmax 
                                           & LATITUDE >= domain$latmin & LATITUDE <= domain$latmax)
+    } 
+    
+    if (timestep %in% c("subhourly", "hourly")) {
       
       # only those in the time period requested should be selected
       dat$POSIXct <- as.POSIXct(paste0(dat$UTC_DATE, dat$UTC_TIME), format = "%Y%m%d%H%M", tz = "UTC")
@@ -198,196 +248,45 @@ GetCRN <- function(gageNames = NULL, siteIds= NULL, elements = NULL, timestep = 
       
       # only those elements requested to be returned
       if (!any(is.null(elements))) dat <- dat[, c(names(dat)[1:8],elements,"POSIXct")]
-      return(data.table::as.data.table(dat))
-    }
-    
-    # The parallel option does not work properly, the FTP server sends and error
-    if (parallel){
-      datTotal <- foreach::"%dopar%"(foreach::foreach(file = filenames, .combine=function(...) data.table::rbindlist(list(...))), {
-        print(paste0("reading this file :",file))
-        dat <- GetsubHourlyFile(file)
-        return(dat)
-      })
-    }else{
-      datTotal <- foreach::"%do%"(foreach::foreach(file = filenames, .combine=function(...) data.table::rbindlist(list(...))), {
-        print(paste0("reading this file :",file))
-        dat <- GetsubHourlyFile(file)
-        return(dat)
-      })
-    }
-  }else if (timestep == "hourly"){
-    
-    # reading the hear information which is distributed in a separate file
-    headerInfo <- read.table(paste0(urlHourly,"/HEADERS.txt"), stringsAsFactors = FALSE) 
-    
-    urlPaths <- paste0(urlHourly,years,"/")
-    
-    # get the name of all the files under the requested years
-    filenames <- foreach::"%do%"(foreach::foreach(url = urlPaths, .combine=function(...) data.table::rbindlist(list(...))), {
-      #  print(url)
-      filenames <- RCurl::getURL(url, dirlistonly = TRUE)
-      filenames <- paste0(url,strsplit(filenames, "\r*\n")[[1]])  # split the name of the files
-      return(filenames)
-    })
-    
-    # To remove other files rather than the CRN files
-    filenames <- unique(grep("CRN", filenames, value=TRUE))
-    
-    # if part of the file names has been given, find only those files and open those
-    if (!is.null(gageNames)) filenames <- unique(grep(paste(gageNames,collapse="|"), filenames, value=TRUE))
-    
-    GetHourlyFile <- function(file){
       
-      dat <- tryCatch(readr::read_table(file, col_names = FALSE, col_types = paste(c(rep('c', 6), rep('d', 32)), collapse = '')),
-                      error=function(cond) {message(cond); return(NA)})
-      names(dat) <- headerInfo[2,]
+    } else if (timestep == "daily") {
       
-      # only those with the requested siteIds to be selected
-      if (!is.null(siteIds)) dat <- subset(dat, WBANNO %in% siteIds )
-      
-      # only those in the requested domain to be selected
-      if (!is.null(domain)) dat <- subset(dat, LONGITUDE >= domain$lonmin & LONGITUDE <= domain$lonmax 
-                                          & LATITUDE >= domain$latmin & LATITUDE <= domain$latmax)
-      
-      # only those in the time period requested should be selected
-      dat$POSIXct <- as.POSIXct(paste0(dat$UTC_DATE, dat$UTC_TIME), format = "%Y%m%d%H%M", tz = "UTC")
-      dat <- subset(dat, POSIXct >= startDate & POSIXct <= endDate)
-      
-      # only those elements requested to be returned
-      if (!any(is.null(elements))) dat <- dat[, c(names(dat)[1:8],elements,"POSIXct")]
-      return(data.table::as.data.table(dat))
-      
-    }
-    
-    # The parallel option does not work properly, the FTP server sends and error
-    if (parallel){
-      datTotal <- foreach::"%dopar%"(foreach::foreach(file = filenames, .combine=function(...) data.table::rbindlist(list(...))), {
-        print(paste0("reading this file :",file))
-        dat <- GetHourlyFile(file)
-        return(dat)
-      })
-    }else{
-      datTotal <- foreach::"%do%"(foreach::foreach(file = filenames, .combine=function(...) data.table::rbindlist(list(...))), {
-        print(paste0("reading this file :",file))
-        dat <- GetHourlyFile(file)
-        return(dat)
-      })
-    }
-  }else if (timestep == "daily"){
-    
-    # reading the hear information which is distributed in a separate file
-    headerInfo <- read.table(paste0(urlDaily,"/HEADERS.txt"), stringsAsFactors = FALSE) 
-    
-    urlPaths <- paste0(urlDaily,years,"/")
-    
-    # get the name of all the files under the requested years
-    filenames <- foreach::"%do%"(foreach::foreach(url = urlPaths, .combine=function(...) data.table::rbindlist(list(...))), {
-      #  print(url)
-      filenames <- RCurl::getURL(url, dirlistonly = TRUE)
-      filenames <- paste0(url,strsplit(filenames, "\r*\n")[[1]])  # split the name of the files
-      return(filenames)
-    })
-    
-    # to remove other files rather than the CRN files
-    filenames <- unique(grep("CRN", filenames, value=TRUE))
-    
-    # if part of the file names has been given, find only those files and open those
-    if (!is.null(gageNames)) filenames <- unique(grep(paste(gageNames,collapse="|"), filenames, value=TRUE))
-    
-    GetDailyFile <- function(file){
-      
-      dat <- tryCatch(readr::read_table(file, col_names = FALSE, col_types = paste(c(rep('c', 3), rep('d', 25)), collapse = '')),
-                      error=function(cond) {message(cond); return(NA)})
-      names(dat) <- headerInfo[2,]
-      
-      # only those with the requested siteIds to be selected
-      if (!is.null(siteIds)) dat <- subset(dat, WBANNO %in% siteIds )
-      
-      # only those in the requested domain to be selected
-      if (!is.null(domain)) dat <- subset(dat, LONGITUDE >= domain$lonmin & LONGITUDE <= domain$lonmax 
-                                          & LATITUDE >= domain$latmin & LATITUDE <= domain$latmax)
-      
-      # only those in the time period requested should be selected
       dat$Date <- as.Date(paste0(dat$LST_DATE), format = "%Y%m%d", tz = "UTC")
       dat <- subset(dat, Date >= as.Date(startDate) & Date <= as.Date(endDate))
       
       # only those elements requested to be returned
       if (!any(is.null(elements))) dat <- dat[, c(names(dat)[1:5],elements)]
-      return(data.table::as.data.table(dat))
-    }
-    
-    # The parallel option does not work properly, the FTP server sends and error
-    if (parallel){
-      datTotal <- foreach::"%dopar%"(foreach::foreach(file = filenames, .combine=function(...) data.table::rbindlist(list(...))), {
-        print(paste0("reading this file :",file))
-        dat <- GetDailyFile(file)
-        return(dat)
-      })
-    }else{
-      datTotal <- foreach::"%do%"(foreach::foreach(file = filenames, .combine=function(...) data.table::rbindlist(list(...))), {
-        print(paste0("reading this file :",file))
-        dat <- GetDailyFile(file)
-        return(dat)
-      })
-    }
-  }else if (timestep == "monthly"){
-    
-    # reading the hear information which is distributed in a separate file
-    headerInfo <- read.table(paste0(urlMonthly,"/HEADERS.txt"), stringsAsFactors = FALSE) 
-    
-    urlPaths <- paste0(urlMonthly)
-    
-    # get the name of all the files under the requested years
-    filenames <- foreach::"%do%"(foreach::foreach(url = urlPaths, .combine=function(...) data.table::rbindlist(list(...))), {
-      #  print(url)
-      filenames <- RCurl::getURL(url, dirlistonly = TRUE)
-      filenames <- paste0(url,strsplit(filenames, "\r*\n")[[1]])  # split the name of the files
-      return(filenames)
-    })
-    
-    # the monthly files have some other file rather than the CRN files, so remove those files
-    filenames <- unique(grep(paste("CRN",collapse="|"), filenames, value=TRUE))
-    
-    # if part of the file names has been given, find only those files and open those
-    if (!is.null(gageNames)) filenames <- unique(grep(paste(gageNames,collapse="|"), filenames, value=TRUE))
-    
-    GetMonthlyFile <- function(file){
       
-      dat <- tryCatch(readr::read_table(file, col_names = FALSE, col_types = paste(c(rep('c', 3), rep('d', 12)), collapse = '')),
-                      error=function(cond) {message(cond); return(NA)})
+    } else if (timestep == "monthly") {
       
-      names(dat) <- headerInfo[2,]
-      
-      # only those with the requested siteIds to be selected
-      if (!is.null(siteIds)) dat <- subset(dat, WBANNO %in% siteIds )
-      
-      # only those in the requested domain to be selected
-      if (!is.null(domain)) dat <- subset(dat, PRECISE_LONGITUDE >= domain$lonmin & PRECISE_LONGITUDE <= domain$lonmax 
-                                          & PRECISE_LATITUDE >= domain$latmin & PRECISE_LATITUDE <= domain$latmax)
-      
-      # only those in the time period requested should be selected
       dat$Date <- as.Date(paste0(dat$LST_YRMO, "01"), format = "%Y%m%d", tz = "UTC")
       dat <- subset(dat, Date >= as.Date(startDate) & Date <= as.Date(endDate))
       
       # only those elements requested to be returned
       if (!any(is.null(elements))) dat <- dat[, c(names(dat)[1:5],elements)]
-      return(data.table::as.data.table(dat))
+      
     }
+    return(data.table::as.data.table(dat))
+  }
+  
+  # The parallel option does not work properly, the FTP server sends and error
+  if (parallel){
     
-    # The parallel option does not work properly, the FTP server sends and error
-    if (parallel){
-      datTotal <- foreach::"%dopar%"(foreach::foreach(file = filenames, .combine=function(...) data.table::rbindlist(list(...))), {
-        print(paste0("reading this file :",file))
-        dat <- GetMonthlyFile(file)
-        return(dat)
-      })
-    }else{
-      datTotal <- foreach::"%do%"(foreach::foreach(file = filenames, .combine=function(...) data.table::rbindlist(list(...))), {
-        print(paste0("reading this file :",file))
-        dat <- GetMonthlyFile(file)
-        return(dat)
-      })
-    }
+    datTotal <- foreach::"%dopar%"(foreach::foreach(file = filenames, .combine=function(...) data.table::rbindlist(list(...))), {
+      print(paste0("reading this file :",file))
+      dat <- ReadTheFile(file)
+      return(dat)
+    })
+    
+  } else {
+    
+    datTotal <- foreach::"%do%"(foreach::foreach(file = filenames, .combine=function(...) data.table::rbindlist(list(...))), {
+      print(paste0("reading this file :",file))
+      dat <- ReadTheFile(file)
+      return(dat)
+    })
+    
   }
   return(datTotal)
 }
+
